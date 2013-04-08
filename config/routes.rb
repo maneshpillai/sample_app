a88af408acfdf6f738421dbafd9b31840583de0a
Manesh Pillai <mpillai1@umbc.edu>
2013-04-08 00:01:51 +0000

    Finish Sign In - After Exercise

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ SampleApp::Application.routes.draw do
   #get "users/new"
   
 	resources :users
-
+	resources :sessions, only: [:new, :create, :destroy]
+	
 	root to: 'static_pages#home'
 	
 	match '/signup', to: 'users#new'
@@ -12,7 +13,9 @@ SampleApp::Application.routes.draw do
 	match '/about', to: 'static_pages#about'
 	match '/contact', to: 'static_pages#contact'
 
-	
+	match '/signup', to: 'users#new'
+	match '/signin', to: 'sessions#new'
+	match '/signout', to: 'sessions#destroy', via: :delete
 	
   # The priority is based upon order of creation:
   # first created -> highest priority.
